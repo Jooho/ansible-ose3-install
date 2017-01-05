189ee4a2fa4caa4b275cfa48f82f635c1c4af82e
jooho <jooho@localhost.localdomain>
2017-01-04 19:02:31 -0500

    install_ocp.sh

--- shell/install_ocp.sh
+++ shell/install_ocp.sh
@@ -1,6 +1,6 @@
 
 # IP information about master/node/etcd/lb/infra
-export hosts=$(grep "example.com" /etc/ansible/hosts | awk -F "public_network_ip=" '{print $2}'|cut -d" " -f1|grep -v "^$" |awk '{print  $1 " "  }')
+export hosts=$(grep "example.com" /etc/ansible/hosts |grep -v subdomain| awk '{print $1}'|sort|uniq)
 
  
 #ssh-copy-id to each hosts
@@ -9,8 +9,9 @@ read password
 echo "~/.ssh/id_rsa.pub file will be copyed to : "
 echo " $hosts"
 for host in $hosts; do
- 	 sshpass -p $password ssh-copy-id -i  ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no root@$host
-
+            sshpass -p $passwordword ssh-copy-id -i  ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no root@$host
+done
 
 
 ansible-playbook  /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml -vvvvv
+
